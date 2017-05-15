ffcc3863423efd31a74520ce6a3648d6a3b80538
scrumbock <scrumboc@yahoo.com>
2017-05-15 17:44:04 -0400

    adding pages to site

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
+
+from = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
 
 
 get '/' do
@@ -8,11 +22,25 @@ get '/' do
 end
 
 get '/contacts' do
-
 erb :contacts
 end
 
 
+get '/index' do
+	erb :index
+end
+
+get '/layout' do
+	erb :layout
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/gallery' do
+	erb :gallery
+end
 
 
 # Business website using Sinatra - Group or Individual
